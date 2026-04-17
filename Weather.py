a59c5fb8e3b6a7a83c1cf83f8daf1f7c17695b20
Olibbbb <jakecanagasuriam@gmail.com>
2026-04-17 12:28:11 +0100

    updated weathers

--- Weather.py
+++ Weather.py
@@ -2,12 +2,12 @@ class Day:
     def __init__():
         Day.currentDay = 0
         Day.currentWeather = None
-        Day.weatherProbabilities = { #weather name and associated probability of blizzard
-            "promising" : 0.25,
-            "clear" : 0.2,
-            "hopeful" : 0.3,
-            "mixed" : 0.5,
-            "dreary" : 0.5,
-            "treacherous" : 0.6,
-            "okay" : 0.25
-        }
+        Day.weathers = [
+            {name: "promising", probability: 0.2, prompt: "The weather is looking promising!"},
+            {name: "okay", probability: 0.25, prompt: "The weather is looking okay."},
+            {name: "hopeful", probability: 0.15, prompt: "The weather is looking hopeful."},
+            {name: "dreary", probability: 0.55, prompt: "The weather is looking dreary..."},
+            {name: "mixed", probability: 0.5, prompt: "The weather is looking mixed."},
+            {name: "uncertain", probability: 0.5, prompt: "The weather is looking uncertain..."},
+            {name: "treacherous", probability: 0.75, prompt: "The weather is looking treacherous..."}
+        ]
